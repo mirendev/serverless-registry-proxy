dd57058da0a7f6531b85b624e84d3058e3872a0c
Paul Hinze <phinze@phinze.com>
2025-07-02 16:00:09 -0500

    Automatic build to Miren's artifact registry

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A development shell for working on Miren Cloud services";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            google-cloud-sdk
+          ];
+        };
+      }
+    );
+}
